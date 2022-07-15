b269856c4611c9a00f0b343f74b944ef7c6d9b90
Alena <al_lucky@mail.ru>
2022-07-16 01:14:10 +0400

    Решение третьей задачи

---
+++ Seminar#3_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Программа выдаёт таблицу кубов чисел от 1 до N");
+Console.WriteLine("");
+Console.Write("Введите число N ");
+double x = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= x)
+ {
+    Console.WriteLine($"Куб числа {count}: {Math.Pow(count, 3)}");
+    count++;
+}
